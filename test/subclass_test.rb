2f1d74627c726e9a388a2d72838a0449454502d4
Martyn Garcia <martyn.garcia@gmail.com>
2008-05-31 10:50:48 -0600

    fixed subclass test

--- test/subclass_test.rb
+++ test/subclass_test.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/test_helper'
 
 context "Extension with class hierachies" do
 
-  xspecify "should make super classes methods available" do
+  specify "should make super classes methods available" do
     Extension.new "subclass" do |e|
       e.sources full_dir("headers/subclass.h")
       node = e.namespace "subclass"
