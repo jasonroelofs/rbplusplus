c585cadb1ba92be65d85cf0013236c7de84e1757
Jason Roelofs <jameskilton@gmail.com>
2009-08-18 12:59:50 -0400

    third test actually tests something, go single mode for now during the rewrite

--- test/extension_test.rb
+++ test/extension_test.rb
@@ -5,6 +5,7 @@ context "Ruby Extension creation" do
   specify "should create a valid Ruby extension" do
     Extension.new "ext_test" do |e|
       e.sources full_dir("headers/empty.h")
+      e.writer_mode :single
     end
 
     should.not.raise LoadError do
@@ -16,6 +17,7 @@ context "Ruby Extension creation" do
     e = Extension.new "extension"
     e.sources full_dir("headers/empty.h")
     e.working_dir = File.join(File.expand_path(File.dirname(__FILE__)), "generated")
+    e.writer_mode :single
     e.build
     e.write
     e.compile
@@ -27,11 +29,14 @@ context "Ruby Extension creation" do
 
   specify "should properly build working dir as deep as needed" do
     should.not.raise Errno::ENOENT do
+      path = File.join(File.expand_path(File.dirname(__FILE__)), "generated", "path1", "path2")
       Extension.new "extension" do |e|
         e.sources full_dir("headers/empty.h")
-        e.working_dir = File.join(File.expand_path(File.dirname(__FILE__)), 
-                                  "generated", "path1", "path2")
+        e.working_dir = path
+        e.writer_mode :single
       end
+
+      assert File.exists?(File.join(path, "extconf.rb"))
     end
   end
 end
