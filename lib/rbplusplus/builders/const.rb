8e8e93843882468d1d1f832829049dd23b05d7f1
Jason Roelofs <jameskilton@gmail.com>
2009-08-29 14:10:17 -0400

    Work around a compiler issue with g++ on Linux

--- lib/rbplusplus/builders/const.rb
+++ lib/rbplusplus/builders/const.rb
@@ -9,8 +9,18 @@ module RbPlusPlus
       end
 
       def write
+        # If this constant is initialized in the header, we need to set the constant to the initialized value
+        # If we just use the variable itself, Linux will fail to compile because the linker won't be able to
+        # find the constant.
+        set_to =
+          if init = code.attributes["init"]
+            init
+          else
+            code.qualified_name
+          end
+
         prefix = parent.rice_variable ? "#{parent.rice_variable}." : "Rice::Module(rb_mKernel)."
-        registrations << "#{prefix}const_set(\"#{code.name}\", to_ruby((int)#{code.qualified_name}));"
+        registrations << "#{prefix}const_set(\"#{code.name}\", to_ruby(#{set_to}));"
       end
 
     end
