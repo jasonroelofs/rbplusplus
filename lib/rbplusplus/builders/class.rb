1e363b46b0a8c0a5609e4589d02b2307ca79594b
Jason Roelofs <jameskilton@gmail.com>
2009-09-01 13:11:26 -0400

    Pulled over the superclass handling into class registration

--- lib/rbplusplus/builders/class.rb
+++ lib/rbplusplus/builders/class.rb
@@ -23,13 +23,28 @@ module RbPlusPlus
         self.rice_variable = "rb_c#{as_variable(code.name)}"
         self.rice_variable_type = "Rice::Data_Type< #{code.qualified_name} >"
 
-        prefix = "#{rice_variable_type} #{rice_variable} = " 
+        prefix = "#{rice_variable_type} #{rice_variable} = "
+
+        supers = self.code.superclasses(:public)
+        superclass = supers[0]
+
+        if supers.length > 1
+          if (superclass = self.code._get_superclass).nil?
+            Logger.warn :mutiple_subclasses, "#{self.code.qualified_name} has multiple public superclasses. " +
+              "Will use first superclass, which is #{supers[0].qualified_name} "
+              "Please use #use_superclass to specify another superclass as needed."
+          end
+        end
+
+        class_names = [self.code.qualified_name]
+        class_names << superclass.qualified_name if superclass && !do_not_wrap?(superclass)
+        class_names = class_names.join(",")
 
         if parent.rice_variable
-          registrations << "#{prefix} Rice::define_class_under< #{code.qualified_name} >" +
+          registrations << "#{prefix} Rice::define_class_under< #{class_names} >" +
                              "(#{parent.rice_variable}, \"#{code.name}\");"
         else
-          registrations << "#{prefix} Rice::define_class< #{code.qualified_name} >(\"#{code.name}\");"
+          registrations << "#{prefix} Rice::define_class< #{class_names} >(\"#{code.name}\");"
         end
       end
 
