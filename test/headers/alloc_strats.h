8c596bd5966464952638d86b2d208e0beecd9498
Jason Roelofs <jameskilton@gmail.com>
2009-09-03 09:57:17 -0400

    Slight test documentation / wording update

--- test/headers/alloc_strats.h
+++ test/headers/alloc_strats.h
@@ -2,12 +2,7 @@
 #define __ALLOC_STRATS_H__
 
 namespace alloc_strats {
-  /**
-   * Why this case could every a good idea, 
-   * or even possible, I have no idea, but
-   * what the hell, it helps with understanding
-   * what allocation_strategy does
-   */
+
   class NoConstructor {
     public:
       ~NoConstructor() { }
