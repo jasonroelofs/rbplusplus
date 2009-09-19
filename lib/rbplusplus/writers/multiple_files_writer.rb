c5f473619f56d82e87a06e0e443e3b27dcdc74fd
Jason Roelofs <jameskilton@gmail.com>
2009-09-18 21:49:26 -0400

    Completely rewrote the multiple-file-writer system.

--- lib/rbplusplus/writers/multiple_files_writer.rb
+++ lib/rbplusplus/writers/multiple_files_writer.rb
@@ -14,23 +14,28 @@ module RbPlusPlus
       # Rice code takes so long to compile, the fewer files one has to compile per change
       # the better.
       def write
-        @to_from_include = ""
+        build_working_dir
+        write_files
+        process_diffs
+        cleanup
+      end
+
+      private
+
+      def build_working_dir
         @build_dir = File.join(working_dir, ".build")
 
         # Build our temp dir
         FileUtils.mkdir_p @build_dir
+      end
 
-        # Write out files
-#        write_to_from_ruby
-        _write_node(builder)
-
-        # Done with writing, commence diff checking
-        Dir["#{@build_dir}/*.rb.*"].each do |file|
+      # Go through each file in @build_dir and check them against
+      # the files in @working_dir, copying over only the ones
+      # that differ
+      def process_diffs
+        Dir["#{@build_dir}/*.{cpp,hpp}"].each do |file|
           FileUtils.cp file, working_dir if files_differ(file)
         end
-
-        # Comparison and move done, remove .build
-        FileUtils.rm_rf @build_dir
       end
 
       def files_differ(file)
@@ -42,161 +47,255 @@ module RbPlusPlus
         !system("diff #{existing} #{new} > #{@build_dir}/diff_info 2>&1")
       end
 
-      # Write out files that include the auto-generated to_/from_ruby constructs.
-      def write_to_from_ruby
-        # Ignore this if there's nothing to write out
-        return if Builders::TypesManager.body.length == 0
+      def cleanup
+        FileUtils.rm_rf @build_dir
+      end
 
-        hpp_file = File.join(@build_dir, "_rbpp_custom.rb.hpp")
-        cpp_file = File.join(@build_dir, "_rbpp_custom.rb.cpp")
+      # Given our builder, go through nodes and write out files
+      def write_files
+        # Couple of steps here:
+        #  - Split up the code into files according to Modules and Classes
+        #  - Header files need a register_#{name} prototype, source needs
+        #    register_#{name}(#{parent}) method definition
+        #  - All includes for this node go in the header.
+        #  - Source gets an include for the related header
+        #  - Save up all data from global_children and at the end write out
+        #    our rbpp_custom files, making sure all header files include
+        #    rbpp_custom.hpp
 
-        @to_from_include = "#include \"#{hpp_file.gsub(/\.build\//, "")}\""
+        @file_writers = []
+        @global_writer = RbppCustomFileWriter.new
 
-        include_guard = "__RICE_GENERATED_RBPP_CUSTOM_HPP__"
+        new_file_for(self.builder)
+        process_globals(self.builder)
 
-        File.open(hpp_file, "w+") do |f|
-          f.puts "#ifndef #{include_guard}"
-          f.puts "#define #{include_guard}"
-          f.puts ""
-          f.puts Builders::TypesManager.includes.uniq.join("\n")
-          f.puts ""
-          f.puts Builders::TypesManager.prototypes.join("\n")
-          f.puts ""
-          f.puts "#endif // #{include_guard}"
+        # Write out from the bottom up, makes sure that children file writers
+        # update their parents as needed
+        @file_writers.each do |fw|
+          fw.write(@build_dir)
         end
 
-        File.open(cpp_file, "w+") do |f|
-          f.puts @to_from_include
-          f.puts ""
-          f.puts Builders::TypesManager.body.join("\n");
+        @global_writer.write(@build_dir)
+      end
+
+      def process_globals(node)
+        # Process the globals
+        node.global_nodes.each do |g|
+          @global_writer << g
+        end
+
+        node.nodes.each do |b|
+          process_globals(b)
         end
       end
 
-      # How this works:
-      #
-      # We split code into files at the top-level class and module nodes. 
-      # Any nested classes / structs / enums, etc, get wrapped in the same
-      # file as the parent class. 
-      def _write_node(node)
-        node.builders.each do |b|
-          if b.is_a?(Builders::ModuleBuilder) ||
-            (b.parent.is_a?(Builders::ModuleBuilder) || b.parent.is_a?(Builders::ExtensionBuilder))
-            _write_node(b) 
-          end
-        end
+      def new_file_for(node, parent = nil)
+        file_writer = FileWriter.new(node, parent)
+        @file_writers << file_writer
 
-        has_parent = node.parent && !node.parent.is_a?(Builders::ExtensionBuilder)
-        is_top = node.is_a?(Builders::ExtensionBuilder)
+        process_file(file_writer, node)
+      end
 
-        filename =
-          if node.class_type
-            node.class_type
-          elsif has_parent && !is_top
-            node.qualified_name
+      # Recursively run through this node's children
+      # adding them to the given file_writer or spawning
+      # off another file writer depending on the type
+      # of node we hit
+      def process_file(file_writer, node)
+        node.nodes.each do |child|
+          if child.is_a?(Builders::ModuleNode) || child.is_a?(Builders::ClassNode)
+            new_file_for(child, file_writer)
           else
-            node.name
+            file_writer << child
+            process_file(file_writer, child) if child.has_children?
           end
+        end
+      end
 
-        # We don't want the top-level extension.rb.cpp file to have an underscore, but
-        # all of the rest should have one
-        if !is_top
-          filename = "_#{filename}"
+      # For every file to write out, we build an instance of a FileWriter here.
+      # This class needs to be given all the nodes it will be writing out to a file
+      #
+      # To handle parents calling register_#{name}() on their children, it's up to the
+      # children writers to inform the parents of their existence
+      class FileWriter
+
+        attr_reader :base_name, :node
+
+        def initialize(node, parent)
+          @node = node
+          @base_name = as_variable(node.qualified_name)
+
+          @header = parent ? "_#{@base_name}.rb.hpp" : nil
+          @source = parent ? "_#{@base_name}.rb.cpp" : "#{@base_name}.cpp"
+          @parent = parent
+
+          @needs_closing = true
+
+          register_with_parent if @parent
+
+          @nodes = [@node]
         end
 
-        filename = filename.functionize
+        # Add a node to this file writer
+        def <<(node)
+          @nodes << node
+        end
 
-        cpp_file = File.join(@build_dir, "#{filename}.rb.cpp")
+        def write(build_dir)
+          @build_dir = build_dir
 
-        if node.parent
-          hpp_file = File.join(@build_dir, "#{filename}.rb.hpp")
-          hpp_include = "#include \"#{hpp_file.gsub(/\.build\//, "")}\""
-          register_func = "register_#{filename}"
+          build_source
+          write_header if @header
+          write_source
+        end
 
-          include_guard = "__RICE_GENERATED_#{filename}_HPP__"
+        def rice_type
+          @node.rice_variable_type
+        end
 
-          register_func_arg = ""
-          register_func_prototype = ""
+        def rice_variable
+          @node.rice_variable
+        end
 
-          if has_parent
-            register_func_arg = node.parent.rice_variable
-            register_func_prototype = "#{node.parent.rice_variable_type} #{register_func_arg}"
+        def has_rice_variable?
+          !@node.rice_variable.nil?
+        end
+
+        def add_register_method(node_name, header)
+          @register_methods ||= []
+          @register_includes ||= []
+
+          @register_includes << "#include \"#{header}\""
+          @register_methods << "register_#{node_name}(#{has_rice_variable? ? self.rice_variable : ""});"
+        end
+
+        protected
+
+        def register_with_parent
+          @register_method = "void register_#{@base_name}"
+          @parent.add_register_method @base_name, @header
+        end
+
+        def build_source
+          @includes = []
+          @declarations = []
+          @registrations = []
+
+          @nodes.each do |node|
+            node.write
+            @includes << node.includes
+            @declarations << node.declarations
+            @registrations << node.registrations
           end
+        end
 
-          # Changes we need to make to the parent for everything to work across multiple
-          # files
-          #
-          # * Add an include to the hpp file
-          # * Add a call to the register method
-          node.parent.includes << hpp_include
+        def parent_signature
+          if @parent && @parent.has_rice_variable?
+            "#{@parent.rice_type} #{@parent.rice_variable}"
+          else
+            ""
+          end
+        end
 
-          # Register for proper flattening of the inheritance tree
-          node.parent.register_node(node, "#{register_func}(#{register_func_arg});")
+        def write_header
+          include_guard = "__RICE_GENERATED_#{@base_name}_HPP__"
 
-          # Modifications to this current node's code:
-          #
-          # * Add a register prototype to the header file
-          # * Set include in node to the header file
-          # * Wrap the body in a register method
-
-          File.open(hpp_file, "w+") do |hpp|
+          File.open(File.join(@build_dir, @header), "w+") do |hpp|
             hpp.puts "#ifndef #{include_guard}"
             hpp.puts "#define #{include_guard}"
-            hpp.puts ""
-            hpp.puts @to_from_include
-            hpp.puts ""
-            hpp.puts "void #{register_func}(#{register_func_prototype});"
-            hpp.puts "#endif"
+            hpp.puts
+
+            if @register_method
+              hpp.puts "#{@register_method}(#{parent_signature});"
+            end
+
+            hpp.puts
+            hpp.puts "#endif // #{include_guard}"
           end
-
-          node.includes << hpp_include
-
-          node.includes = get_includes_for(node)
-
-          node.declarations = get_decls_for(node)
-
-          node.body = [
-            "void #{register_func}(#{register_func_prototype}) {",
-            get_body_for(node),
-            "}"
-          ]
         end
 
-        File.open(cpp_file, "w+") do |cpp|
-          cpp.puts node.to_s
+        def write_source
+          File.open(File.join(@build_dir, @source), "w+") do |cpp|
+            if (incls = @includes.flatten.compact).any?
+              cpp.puts "", incls.uniq.sort.reverse.join("\n"), ""
+            end
+
+            if @register_method
+              cpp.puts "", "#include \"#{@header}\"", ""
+            end
+
+            if @register_includes
+              @register_includes.each do |i|
+                cpp.puts i
+              end
+            end
+
+            cpp.puts "#include \"_rbpp_custom.rb.hpp\""
+
+            if (decls = @declarations.flatten.compact).any?
+              cpp.puts "", decls.join("\n"), ""
+            end
+
+            if @register_method
+              cpp.puts "#{@register_method}(#{parent_signature}) {"
+            end
+
+            if (regs = @registrations.flatten.compact).any?
+              cpp.puts regs.join("\n")
+            end
+
+            if @register_methods
+              @register_methods.each do |m|
+                cpp.puts m
+              end
+            end
+
+            # TODO I really need a better way of handling this
+            if @needs_closing
+              cpp.puts "}"
+            end
+          end
         end
+
+        def as_variable(name)
+          name.gsub(/::/, "_").gsub(/[<>]/, "_").gsub("*", "_ptr_")
+        end
+
       end
 
-      def get_includes_for(node)
-        return node.includes if node.is_a?(Builders::ModuleBuilder)
+      class RbppCustomFileWriter < FileWriter
 
-        includes = 
-          node.builders.inject([]) do |memo, inner|
-            memo << get_includes_for(inner) unless inner.is_a?(Builders::ModuleBuilder)
-            memo
+        def initialize
+          @base_name = "rbpp_custom"
+          @header = "_#{@base_name}.rb.hpp"
+          @source = "_#{@base_name}.rb.cpp"
+          @nodes = []
+          @needs_closing = false
+        end
+
+        protected
+
+        def write_header
+          include_guard = "__RICE_GENERATED_#{@base_name}_HPP__"
+
+          File.open(File.join(@build_dir, @header), "w+") do |hpp|
+            hpp.puts "#ifndef #{include_guard}"
+            hpp.puts "#define #{include_guard}"
+
+            if (incls = @includes.flatten.compact).any?
+              hpp.puts "", incls.uniq.sort.reverse.join("\n"), ""
+            end
+
+            if (decls = @declarations.flatten.compact).any?
+              hpp.puts "", decls.join("\n"), ""
+            end
+            hpp.puts
+            hpp.puts "#endif // #{include_guard}"
           end
-        node.includes + includes.flatten
-      end
 
-      def get_body_for(node)
-        return node.body if node.is_a?(Builders::ModuleBuilder)
+          @declarations = []
+          @includes = []
+        end
 
-        body = 
-          node.builders.inject([]) do |memo, inner|
-            memo << get_body_for(inner) unless inner.is_a?(Builders::ModuleBuilder)
-            memo
-          end
-        node.body + body.flatten
-      end
-
-      def get_decls_for(node)
-        return node.declarations if node.is_a?(Builders::ModuleBuilder)
-
-        declarations = 
-          node.builders.inject([]) do |memo, inner|
-            memo << get_decls_for(inner) unless inner.is_a?(Builders::ModuleBuilder)
-            memo
-          end
-        node.declarations + declarations.flatten
       end
 
     end
